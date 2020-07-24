7b17bd7edd03c2863c61dfad220cfbd5e443de54
shakhawatfci <94shakha.18@gamil.com>
2020-07-24 14:01:40 +0600

    service section added

---
+++ src/App.svelte
@@ -0,0 +1,53 @@
+<script>
+// import { fly } from 'svelte/transition';
+import Services from './components/services/Service.svelte';
+</script>
+
+
+<div class="container-fluid">
+	<div class="row hero-area">
+		<div class="col-md-8 mx-auto">
+
+
+			<h1 class="hero-title">Shakhawat Hossain</h1>
+			<p class="hero-paragraph">
+                Software Engineer , Open Source Contributor , Envato Author
+			</p>
+
+            <ul class="social-network social-circle">
+                <li><a  target="_blank"   href="https://github.com/shakhawatfci" class="icoRss" title="Rss"><i class="fa fa-github"></i></a></li>
+                <li><a  target="_blank"   href="https://facebook.com/snsabbir.fci" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+                <li><a  target="_blank"  href="https://twitter.com/shakhawat_fci" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+                <li><a    href="mailto:94shakha.18@gmail.com" class="icoGoogle" title="Google +"><i class="fa fa-envelope"></i></a></li>
+                <li><a  target="_blank"  href="https://www.linkedin.com/in/shakhawat-hossain-a2206b116/" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+            </ul>	
+		</div>
+	</div>
+
+
+
+</div>
+
+<div class="container">
+   
+
+   <!-- services  -->
+   <Services/>
+   <!-- services  -->
+
+    
+    <!-- expertise  -->
+     <div class="row">
+		<div class="col-md-12 text-center">
+			<h1 class="service-title mt-5">My Expertices</h1>
+		</div>
+	</div>	
+	<!-- expertise  -->
+
+
+
+
+</div>
+
+
+
---
+++ src/components/services/Service.svelte
@@ -0,0 +1,32 @@
+<script type="text/javascript">
+	let services = [
+     { id: 1, serviceName : 'Software Development' , icon : 'fa fa-language' },
+     { id: 2, serviceName : 'Apps Development' , icon : 'fa fa-apple' },
+     { id: 3, serviceName : 'Web Development' , icon : 'fa fa-code' },
+     { id: 4, serviceName : 'Web Design' , icon : 'fa fa-globe' },
+     { id: 5, serviceName : 'SASS PROVIDER' , icon : 'fa fa-cubes' },
+     { id: 6, serviceName : 'Custom Software' , icon : 'fa fa-cogs' }
+	];
+</script>
+
+		<!-- what i do  -->
+
+     <div class="row">
+		<div class="col-md-12 text-center">
+			<h1 class="service-title mt-5">What I Do</h1>
+		</div>
+	</div>		
+     <div class="row">
+       {#each services as value}
+       	  <div class="col-md-4">
+     		<div class="work-section">
+     			<h3 class="work-icon">
+     				<i class={value.icon} aria-hidden="true"></i>
+     			</h3>
+     			<h3 class="work-title">{value.serviceName}</h3>
+     		</div>
+     	</div>
+       {/each}
+     	
+     </div>
+	<!-- what i do  -->
